12305869c3a865bf20ee2f83a7afdbab626e8d41
Ruth <ruro122020@gmail.com>
2023-09-26 17:17:35 -0400

    fetch games from API

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ let options = {
     method: 'GET',
     headers: config
 }
-function getGames(){}
+function getGames(){
+    fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options)
+    .then(res => res.json())
+    .then(games => console.log('games', games))
+}
 
 //initialize
 function init(){
