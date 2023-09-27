8de3ee8ddfb625e4a5d16cf08a177a834918a042
Ruth <ruro122020@gmail.com>
2023-09-26 23:07:43 -0400

    add event to game title

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ let gamesList;
 let counter = 0;
 /***Helper functions */
 
+/***Events */
+function titleEvent(titleElement){
+    titleElement.addEventListener('click', () => {
+        handleTitleEvent(titleElement)
+    })
+}
+/***Handle Events Functions */
+function handleTitleEvent(titleElement){
+    const gameObj = gamesList.find(game => game.title === titleElement.textContent )
+    renderGameInfo(gameObj)
+}
 /***Render to DOM */
 function renderGameTitles (){
     const nextGames = gamesList.map(game => game.title)
@@ -11,6 +22,7 @@ function renderGameTitles (){
         const titleElement = document.createElement('li')
         titleElement.textContent = gameTitle
         titlesContainer.appendChild(titleElement)
+        titleEvent(titleElement)
     }) 
 }   
 /***Fetch Requests */
